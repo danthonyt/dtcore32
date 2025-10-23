eda6b60b1eb20374decb8ef2e436f173ccd8df43
David Torres <danthonyt@proton.me>
2025-10-23 16:17:07 -0700

    moved all soc files to another repo, this repo now only contains cpu core files

--- src/software/dtcore_time.c
+++
@@ -1,7 +0,0 @@
-#include "dtcore_time.h"
-uint32_t timer_get_cycles(void)
-{
-    volatile uint32_t cycles;
-    asm volatile("csrr %0, mcycle" : "=r"(cycles));
-    return cycles;
-}
--- src/software/dtcore_time.h
+++
@@ -1,9 +0,0 @@
-#ifndef DTCORE_TIME_H
-#define DTCORE_TIME_H
-
-#include <stdint.h>
-
-// timer API
-uint32_t timer_get_cycles(void);
-
-#endif // DTCORE_TIME_H
